863dcb7615ebdfa0179223f9f27c75163680c204
HSpotlight <pakawat.ane@gmail.com>
2020-06-05 22:34:30 +0700

    MF-001 Add calculation logic for MRT blue line

--- src/services/fare.service.ts
+++ src/services/fare.service.ts
@@ -1,8 +1,15 @@
 import { METRO_STATION } from "../types/MetroStation";
+import { MRT_BLUE_LINE } from "../data/MrtBlueLine";
+import { MRT_BLUE_FARE } from "../common/constants";
 
 export const FareService = {
     calculate(source: METRO_STATION, destination: METRO_STATION): number {
-        // TODO: implements
-        return 0;
+        const sourceIndex = MRT_BLUE_LINE.line.indexOf(source);
+        const destinationIndex = MRT_BLUE_LINE.line.indexOf(destination);
+        const hops = Math.abs(sourceIndex - destinationIndex);
+        if (hops > 13) {
+            return MRT_BLUE_FARE[13];
+        }
+        return MRT_BLUE_FARE[hops];
     }
 }
--- src/services/tests/fare.service.test.ts
+++ src/services/tests/fare.service.test.ts
@@ -1,16 +1,72 @@
 import { FareService } from '../fare.service';
 import { MRT_BLUE_STATION } from '../../types/MetroStation';
 
-jest.unmock('../fare.service');
+
 
 describe('FareService', () => {
-    it('should call with source and destination', () => {
-        FareService.calculate = jest.fn();
-        const source = MRT_BLUE_STATION.BANG_KHAE;
-        const destination = MRT_BLUE_STATION.BANG_KHUN_NON;
+    it('should return 16 when source and destination are the same station', () => {
+        const source = MRT_BLUE_STATION.LAT_PHRAO;
+        const destination = MRT_BLUE_STATION.LAT_PHRAO;
 
-        FareService.calculate(source, destination);
+        const fare = FareService.calculate(source, destination);
 
-        expect(FareService.calculate).toBeCalledWith(source, destination);
+        expect(fare).toBe(16);
+    });
+    it('should return 16 when distance from source-distance is 1 hop', () => {
+        const source = MRT_BLUE_STATION.LAT_PHRAO;
+        const destination = MRT_BLUE_STATION.RATCHADAPHISEK;
+
+        const fare = FareService.calculate(source, destination);
+
+        expect(fare).toBe(16);
+    });
+    it('should return 19 when distance from source-distance is 2 hops', () => {
+        const source = MRT_BLUE_STATION.LAT_PHRAO;
+        const destination = MRT_BLUE_STATION.SUTTHISAN;
+
+        const fare = FareService.calculate(source, destination);
+
+        expect(fare).toBe(19);
+    });
+
+    it('should return 21 when distance from source-distance is 3 hops', () => {
+        const source = MRT_BLUE_STATION.LAT_PHRAO;
+        const destination = MRT_BLUE_STATION.HUAI_KHWANG;
+
+        const fare = FareService.calculate(source, destination);
+
+        expect(fare).toBe(21);
+    });
+    it('should return 23 when distance from source-distance is 4 hops', () => {
+        const source = MRT_BLUE_STATION.LAT_PHRAO;
+        const destination = MRT_BLUE_STATION.THAILAND_CULTURAL_CENTRE;
+
+        const fare = FareService.calculate(source, destination);
+
+        expect(fare).toBe(23);
+    });
+    it('should return 26 when distance from source-distance is 5 hops', () => {
+        const source = MRT_BLUE_STATION.LAT_PHRAO;
+        const destination = MRT_BLUE_STATION.PHRA_RAM_9;
+
+        const fare = FareService.calculate(source, destination);
+
+        expect(fare).toBe(26);
+    });
+    it('should return 42 when distance from source-distance is 13 hops', () => {
+        const source = MRT_BLUE_STATION.LAT_PHRAO;
+        const destination = MRT_BLUE_STATION.HUA_LAMPHONG;
+
+        const fare = FareService.calculate(source, destination);
+
+        expect(fare).toBe(42);
+    });
+    it('should return 42 when distance from source-distance more than 13 hops', () => {
+        const source = MRT_BLUE_STATION.LAT_PHRAO;
+        const destination = MRT_BLUE_STATION.SANAM_CHAI;
+
+        const fare = FareService.calculate(source, destination);
+
+        expect(fare).toBe(42);
     });
 });
