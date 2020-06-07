fb879003773017afbf5ec80175aaf5eadf8ae1e2
HSpotlight <pakawat.ane@gmail.com>
2020-06-07 13:48:04 +0700

    MF-000 show travel route and fare

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,23 +1,34 @@
 import React, { useState, useEffect } from "react";
 import { FareService, TravelRoute } from "../services/fare.service";
 import { METRO_STATION, MRT_BLUE_STATION } from "../types/MetroStation";
-import { Button, Select, InputLabel, FormControl, FormHelperText } from "@material-ui/core";
-import '../styles/Calculator.scss';
+import {
+  Button,
+  Select,
+  InputLabel,
+  FormControl,
+  FormHelperText,
+} from "@material-ui/core";
+import "../styles/Calculator.scss";
 
 const Calculator = () => {
-  const [source, setSource] = useState<string>('');
-  const [destination, setDestination] = useState<string>('');
-  const [travelRoute, setTravelRoute] = useState<TravelRoute | undefined>(undefined);
+  const [source, setSource] = useState<string>("");
+  const [destination, setDestination] = useState<string>("");
+  const [travelRoute, setTravelRoute] = useState<TravelRoute | undefined>(
+    undefined
+  );
   const [isFormValid, setFormValid] = useState<boolean>(false);
 
   const calculateRoute = () => {
-    const travelRoute = FareService.calculate(source as METRO_STATION, destination as METRO_STATION);
+    const travelRoute = FareService.calculate(
+      source as METRO_STATION,
+      destination as METRO_STATION
+    );
     setTravelRoute(travelRoute);
   };
 
   const resetForm = () => {
-    setSource('');
-    setDestination('');
+    setSource("");
+    setDestination("");
     setTravelRoute(undefined);
   };
 
@@ -51,14 +62,21 @@ const Calculator = () => {
       {travelRoute !== undefined && CalculationResult(travelRoute)}
     </section>
   );
-}
+};
 
-const SelectComponent = ({ title, value, onChange}: {title: string, value: string, onChange: Function}) => {
-
+const SelectComponent = ({
+  title,
+  value,
+  onChange,
+}: {
+  title: string;
+  value: string;
+  onChange: Function;
+}) => {
   const selectElementId = title + "-native-required";
   const getLabel = (key: string): string => {
     return key.replace(/_/g, " ");
-  }
+  };
 
   return (
     <FormControl required>
@@ -85,11 +103,28 @@ const SelectComponent = ({ title, value, onChange}: {title: string, value: strin
       <FormHelperText>Required</FormHelperText>
     </FormControl>
   );
-}
+};
 
 function CalculationResult(travelRoute: TravelRoute): React.ReactNode {
-  return <div>Fare: {travelRoute.fare}</div>;
+  return (
+    <div>
+      <div>Fare: {travelRoute.fare}</div>
+      <div className="travel-route-container">
+        {travelRoute.route.map((stationKey, index) => {
+          const dashline = <div className="mrt-blue-dotted-line"></div>;
+          return (
+            <section key={stationKey}>
+              {index > 0 && dashline}
+              <section className="station-container">
+                <div className="mrt-blue-icon"></div>
+                <div>{stationKey}</div>
+              </section>
+            </section>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Calculator;
-
