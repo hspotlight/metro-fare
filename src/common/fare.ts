9df1f03686f111fef26bb9c509c719ae7a5bc29b
HSpotlight <pakawat.ane@gmail.com>
2020-06-14 23:42:13 +0700

    MF-003 refactor fare constants

--- src/common/fare.ts
+++ src/common/fare.ts
@@ -1,9 +1,15 @@
-export const BRT_FARE = [15];
+export const METRO_FARE: FARE = {
+    'MRT_BLUE': [16, 16, 19, 21, 23, 25, 28, 30, 32, 35, 37, 39, 42, 42],
+    'BRT': [15],
+    'ARL': [15, 15, 20, 25, 30, 35, 40, 45],
+    'BTS': [16, 16, 23, 26, 30, 33, 37, 40, 44, 44, 44, 44, 44, 44],
+    'MRT_PURPLE': [14, 17, 20, 23, 25, 27, 30, 33, 36, 38, 40, 42, 42, 42],
+}
 
-export const ARL_FARE = [15, 15, 20, 25, 30, 35, 40, 45];
-
-export const BTS_FARE = [16, 16, 23, 26, 30, 33, 37, 40, 44, 44, 44, 44, 44, 44];
-
-export const MRT_BLUE_FARE = [16, 16, 19, 21, 23, 25, 28, 30, 32, 35, 37, 39, 42, 42];
-
-export const MRT_PURPLE_FARE = [14, 17, 20, 23, 25, 27, 30, 33, 36, 38, 40, 42, 42, 42];
+export type FARE = {
+    BRT: number[],
+    ARL: number[],
+    BTS: number[],
+    MRT_BLUE: number[],
+    MRT_PURPLE: number[],
+}
