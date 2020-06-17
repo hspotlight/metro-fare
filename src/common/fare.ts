ee1c113e5e8d79187a7b6364c2c7f0ea49aba26a
HSpotlight <pakawat.ane@gmail.com>
2020-06-17 22:26:44 +0700

    MF-003 Add fare type for BTS extension 15 baht

--- src/common/fare.ts
+++ src/common/fare.ts
@@ -2,6 +2,7 @@ export const METRO_FARE: FARE = {
     'BRT': [15],
     'ARL': [15, 15, 20, 25, 30, 35, 40, 45],
     'BTS': [16, 16, 23, 26, 30, 33, 37, 40, 44, 44, 44, 44, 44, 44],
+    'BTS_EXTENSION_15': [15],
     'MRT_BLUE': [16, 16, 19, 21, 23, 25, 28, 30, 32, 35, 37, 39, 42, 42],
     'MRT_PURPLE': [14, 17, 20, 23, 25, 27, 30, 33, 36, 38, 40, 42, 42, 42],
 }
@@ -10,6 +11,7 @@ export type FARE = {
     BRT: number[],
     ARL: number[],
     BTS: number[],
+    BTS_EXTENSION_15: number[],
     MRT_BLUE: number[],
     MRT_PURPLE: number[],
 }
