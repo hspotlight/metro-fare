e91fa329035c3cff7612db4e71df57ff5f901a61
HSpotlight <pakawat.ane@gmail.com>
2020-06-17 11:53:23 +0700

    MF-000 hotfix fare NaN

--- src/services/fare.service.ts
+++ src/services/fare.service.ts
@@ -34,13 +34,13 @@ export const FareService = {
     },
     calculateTotalFareFromRoute(routeSegment: RouteSegment, hasMoreThanOneSegment = false): number {
         const fareTable: number[] = METRO_FARE[routeSegment.fareType];
-        
+
         const hops = routeSegment.route.length - 1;
         if (hops === 0 && hasMoreThanOneSegment) {
             return 0;
         }
 
-        if (hops > fareTable.length) {
+        if (hops > fareTable.length - 1) {
             return fareTable[fareTable.length - 1];
         }
 
