87bbb075d76e525f3f5137f95425d3692dd6b8ad
HSpotlight <pakawat.ane@gmail.com>
2020-06-17 22:26:44 +0700

    MF-003 Add fare type for BTS extension 15 baht

--- src/types/FareType.ts
+++ src/types/FareType.ts
@@ -2,5 +2,7 @@ export enum FareType {
     BRT = 'BRT',
     ARL = 'ARL',
     BTS = 'BTS',
+    BTS_EXTENSION_15 = 'BTS_EXTENSION_15',
     MRT_BLUE = 'MRT_BLUE',
+    MRT_PURPLE = 'MRT_PURPLE',
 }
