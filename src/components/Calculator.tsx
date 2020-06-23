4b54eb9e864e7cf087a903f1c3fdf4edcc82cdd3
HSpotlight <pakawat.ane@gmail.com>
2020-06-23 21:27:30 +0700

    MF-004 Extract StationSelect

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -7,18 +7,13 @@ import {
   FormHelperText,
 } from "@material-ui/core";
 import { FareService, TravelRoute } from "../services/fare.service";
-import {
-  METRO_STATION,
-  BTS_SILOM_STATION,
-  LineType,
-} from "../types";
-import { STATIONS, Station } from "../data/Stations";
+import { METRO_STATION } from "../types";
 import { FindRouteMethod } from "../services/graph.service";
 import CalculationResult from "./CalculationResult";
-import "../styles/Calculator.scss";
-import { getStationName } from "../services/util.service";
 import { useTranslation } from "react-i18next";
 import LanguageController from "./LanguageController";
+import StationSelect from "./StationSelect";
+import "../styles/Calculator.scss";
 
 const Calculator = () => {
   const { t: translate } = useTranslation();
@@ -28,7 +23,9 @@ const Calculator = () => {
   const [travelRoute, setTravelRoute] = useState<TravelRoute | undefined>(
     undefined
   );
-  const [findRouteMethod, setFindRouteMethod] = useState<FindRouteMethod>('lowestHop');
+  const [findRouteMethod, setFindRouteMethod] = useState<FindRouteMethod>(
+    "lowestHop"
+  );
   const [isFormInvalid, setFormInValid] = useState<boolean>(false);
 
   const calculateRoute = () => {
@@ -44,12 +41,12 @@ const Calculator = () => {
     setSource("");
     setDestination("");
     setErrorMessage("");
-    setFindRouteMethod('lowestHop');
+    setFindRouteMethod("lowestHop");
     setTravelRoute(undefined);
   };
 
   useEffect(() => {
-    const isFormValid = (source.length === 0 || destination.length === 0);
+    const isFormValid = source.length === 0 || destination.length === 0;
     setFormInValid(isFormValid);
   }, [source, destination]);
 
@@ -59,35 +56,44 @@ const Calculator = () => {
         <h1>Metro Fare</h1>
         <LanguageController />
       </section>
-      <SelectStationComponent
-        title={translate('route.source')}
+      <StationSelect
+        title={translate("route.source")}
         value={source}
         onChange={setSource}
       />
-      <SelectStationComponent
-        title={translate('route.destination')}
+      <StationSelect
+        title={translate("route.destination")}
         value={destination}
         onChange={setDestination}
       />
       <FormControl style={{ width: "100%" }} required>
-        <InputLabel htmlFor={'find-route-method'}>{translate('findRouteMethod.method')}</InputLabel>
+        <InputLabel htmlFor={"find-route-method"}>
+          {translate("findRouteMethod.method")}
+        </InputLabel>
         <Select
           native
-          onChange={(e: any) => setFindRouteMethod(e.target.value as FindRouteMethod)}
+          onChange={(e: any) =>
+            setFindRouteMethod(e.target.value as FindRouteMethod)
+          }
           name={"Line"}
           value={findRouteMethod}
           inputProps={{
-            id: 'find-route-method',
+            id: "find-route-method",
           }}
         >
-          <option value={"lowestHop"}>{translate('findRouteMethod.lowestHop')}</option>
-          <option value={'lowestFare'}>{translate('findRouteMethod.lowestFare')}</option>
+          <option value={"lowestHop"}>
+            {translate("findRouteMethod.lowestHop")}
+          </option>
+          <option value={"lowestFare"}>
+            {translate("findRouteMethod.lowestFare")}
+          </option>
         </Select>
-        <FormHelperText>{translate('common.required')}</FormHelperText>
+        <FormHelperText>{translate("common.required")}</FormHelperText>
       </FormControl>
+
       <section className="form-button-group">
         <Button variant="contained" color="secondary" onClick={resetForm}>
-        {translate('common.reset')}
+          {translate("common.reset")}
         </Button>
         <Button
           variant="contained"
@@ -96,7 +102,7 @@ const Calculator = () => {
           style={{ marginLeft: "20px" }}
           disabled={isFormInvalid}
         >
-          {translate('common.search')}
+          {translate("common.search")}
         </Button>
       </section>
       {errorMessage.length > 0 && <ErrorMessage errorMessage={errorMessage} />}
@@ -107,79 +113,6 @@ const Calculator = () => {
 
 const ErrorMessage = ({ errorMessage }: { errorMessage: string }) => {
   return <div className="form-error-message">{errorMessage}</div>;
-};
-
-const SelectStationComponent = ({
-  title,
-  value,
-  onChange,
-}: {
-  title: string;
-  value: string;
-  onChange: Function;
-}) => {
-  const { t: translate, i18n } = useTranslation();
-  const [lineType, setLineType] = useState<LineType>(LineType.MRT_BLUE);
-  const lineElementId = title + "-line-native-required";
-  const selectElementId = title + "-native-required";
-  const stationsName = STATIONS.filter(station => station.lineType === lineType && !station.isNotAvailable);
-
-  const handleLineTypeSelectChange = (value: string) => {
-    setLineType(value as LineType);
-    onChange("");
-  };
-
-  useEffect(() => {
-    if (Object.values(BTS_SILOM_STATION).find((btsKey => btsKey === value))) {
-      setLineType(LineType.BTS_SILOM);
-    } else if (value.length !== 0) {
-      setLineType(LineType.MRT_BLUE);
-    }
-  }, [value]);
-
-  return (
-    <section>
-      <FormControl style={{ width: "145px" }} required>
-        <InputLabel htmlFor={lineElementId}>{translate('lineType.line')}</InputLabel>
-        <Select
-          native
-          onChange={(e: any) => handleLineTypeSelectChange(e.target.value)}
-          name={"Line"}
-          value={lineType}
-          inputProps={{
-            id: lineElementId,
-          }}
-        >
-          <option value={"MRT_BLUE"}>{translate('lineType.mrtBlue')}</option>
-          <option value={"BTS_SILOM"}>{translate('lineType.btsSilom')}</option>
-        </Select>
-        <FormHelperText>{translate('common.required')}</FormHelperText>
-      </FormControl>
-      <FormControl style={{ width: "calc(100% - 145px)" }} required>
-        <InputLabel htmlFor={selectElementId}>{title}</InputLabel>
-        <Select
-          native
-          onChange={(e) => onChange(e.target.value)}
-          name={title}
-          value={value}
-          inputProps={{
-            id: selectElementId,
-          }}
-        >
-          <option value="" disabled></option>
-          {stationsName.map((station: Station) => {
-            const label = "(" + station.key + ") " + getStationName(station, i18n.language);
-            return (
-              <option key={station.key} value={station.key}>
-                {label}
-              </option>
-            );
-          })}
-        </Select>
-        <FormHelperText>{translate('common.required')}</FormHelperText>
-      </FormControl>
-    </section>
-  );
 };
 
 export default Calculator;
---
+++ src/components/StationSelect.tsx
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import { FormControl, InputLabel, Select, FormHelperText } from "@material-ui/core";
+import { STATIONS, Station } from "../data/Stations";
+import { LineType, BTS_SILOM_STATION } from "../types";
+import { getStationName } from "../services/util.service";
+import "../styles/StationSelect.scss";
+
+const StationSelect = ({
+  title,
+  value,
+  onChange,
+}: {
+  title: string;
+  value: string;
+  onChange: Function;
+}) => {
+  const { t: translate, i18n } = useTranslation();
+  const [lineType, setLineType] = useState<LineType>(LineType.MRT_BLUE);
+  const lineElementId = title + "-line-native-required";
+  const selectElementId = title + "-native-required";
+  const isStationAvailable = (station: Station) => station.lineType === lineType && !station.isNotAvailable
+  const stationsName = STATIONS.filter(isStationAvailable);
+
+  const handleLineTypeSelectChange = (value: string) => {
+    setLineType(value as LineType);
+    onChange("");
+  };
+
+  useEffect(() => {
+    if (Object.values(BTS_SILOM_STATION).find((btsKey) => btsKey === value)) {
+      setLineType(LineType.BTS_SILOM);
+    } else if (value.length !== 0) {
+      setLineType(LineType.MRT_BLUE);
+    }
+  }, [value]);
+
+  return (
+    <section>
+      <FormControl className="line-type-select" required>
+        <InputLabel htmlFor={lineElementId}>
+          {translate("lineType.line")}
+        </InputLabel>
+        <Select
+          native
+          onChange={(e: any) => handleLineTypeSelectChange(e.target.value)}
+          name={"Line"}
+          value={lineType}
+          inputProps={{
+            id: lineElementId,
+          }}
+        >
+          <option value={"MRT_BLUE"}>{translate("lineType.mrtBlue")}</option>
+          <option value={"BTS_SILOM"}>{translate("lineType.btsSilom")}</option>
+        </Select>
+        <FormHelperText>{translate("common.required")}</FormHelperText>
+      </FormControl>
+      <FormControl className="station-select" required>
+        <InputLabel htmlFor={selectElementId}>{title}</InputLabel>
+        <Select
+          native
+          onChange={(e) => onChange(e.target.value)}
+          name={title}
+          value={value}
+          inputProps={{
+            id: selectElementId,
+          }}
+        >
+          <option value="" disabled></option>
+          {stationsName.map((station: Station) => {
+            const label =
+              "(" + station.key + ") " + getStationName(station, i18n.language);
+            return (
+              <option key={station.key} value={station.key}>
+                {label}
+              </option>
+            );
+          })}
+        </Select>
+        <FormHelperText>{translate("common.required")}</FormHelperText>
+      </FormControl>
+    </section>
+  );
+};
+
+export default StationSelect;
