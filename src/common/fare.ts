06bda8920c35d6c464e236fd0b3532993b713474
HSpotlight <pakawat.ane@gmail.com>
2020-06-28 19:58:05 +0700

    MF-005 update fare for BTS sukhumvit line

--- src/common/fare.ts
+++ src/common/fare.ts
@@ -3,6 +3,9 @@ export const METRO_FARE: FARE = {
     'ARL': [15, 15, 20, 25, 30, 35, 40, 45],
     'BTS_SILOM': [16, 16, 23, 26, 30, 33, 37, 40, 44, 44, 44, 44, 44, 44],
     'BTS_SILOM_EXTENSION_15': [15],
+    'BTS_SUKHUMVIT': [16, 16, 23, 26, 30, 33, 37, 40, 44, 44, 44, 44, 44, 44],
+    'BTS_SUKHUMVIT_EXTENSION_15': [15],
+    'BTS_SUKHUMVIT_EXTENSION_0': [0],
     'MRT_BLUE': [16, 16, 19, 21, 23, 25, 28, 30, 32, 35, 37, 39, 42, 42],
     'MRT_PURPLE': [14, 17, 20, 23, 25, 27, 30, 33, 36, 38, 40, 42, 42, 42],
 }
@@ -12,6 +15,9 @@ export type FARE = {
     ARL: number[],
     BTS_SILOM: number[],
     BTS_SILOM_EXTENSION_15: number[],
+    BTS_SUKHUMVIT: number[],
+    BTS_SUKHUMVIT_EXTENSION_15: number[],
+    BTS_SUKHUMVIT_EXTENSION_0: number[],
     MRT_BLUE: number[],
     MRT_PURPLE: number[],
 }
