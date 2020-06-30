e7a66562d9bb49a3d9dc6a495d8c431b5d694b4c
HSpotlight <pakawat.ane@gmail.com>
2020-06-30 22:29:49 +0700

    MF-006 Refactor LegendLabel

--- src/components/map/MapLegend.tsx
+++ src/components/map/MapLegend.tsx
@@ -1,40 +1,40 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
+import React from "react";
+import { useTranslation } from "react-i18next";
 import "../../styles/MapLegend.scss";
 
 const MapLegend = () => {
-    const {t: translate} = useTranslation();
-    return (
-        <div className="map-legend">
-            <div className="title">{translate("map.legend.legend")}</div>
-            <div className="legend">
-                <div className="legend-key-value">
-                    <div className="key mrt-blue-line">
-                        <div className="line"></div>
-                        <div className="circle"></div>
-                        <div className="line"></div>
-                    </div>
-                    <div>{translate("map.legend.mrtBlueLine")}</div>
-                </div>
-                <div className="legend-key-value">
-                    <div className="key bts-silom-line">
-                        <div className="line"></div>
-                        <div className="circle"></div>
-                        <div className="line"></div>
-                    </div>
-                    <div>{translate("map.legend.btsSilomLine")}</div>
-                </div>
-                <div className="legend-key-value">
-                    <div className="key bts-sukhumvit-line">
-                        <div className="line"></div>
-                        <div className="circle"></div>
-                        <div className="line"></div>
-                    </div>
-                    <div>{translate("map.legend.btsSukhumvitLine")}</div>
-                </div>
-            </div>
-        </div>
-    );
-}
+  const { t: translate } = useTranslation();
+  return (
+    <div className="map-legend">
+      <div className="title">{translate("map.legend.legend")}</div>
+      <div className="legend">
+        <LegendLabel
+          line="mrt-blue-line"
+          title={translate("map.legend.mrtBlueLine")}
+        />
+        <LegendLabel
+          line="bts-silom-line"
+          title={translate("map.legend.btsSilomLine")}
+        />
+        <LegendLabel
+          line="bts-sukhumvit-line"
+          title={translate("map.legend.btsSukhumvitLine")}
+        />
+      </div>
+    </div>
+  );
+};
+
+const LegendLabel = ({ line, title }: { line: string; title: string }) => {
+  return (
+    <div className="legend-key-value">
+      <div className={`key ${line}`}>
+        <div className="line"></div>
+        <div className="circle"></div>
+      </div>
+      <div>{title}</div>
+    </div>
+  );
+};
 
 export default MapLegend;
