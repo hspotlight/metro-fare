2aa4ab9641c8f9a37968d683161f7cdb5aa96505
HSpotlight <pakawat.ane@gmail.com>
2020-07-05 00:37:50 +0700

    MF-007 Show top 3 lowest fare routes

--- src/services/fare.service.ts
+++ src/services/fare.service.ts
@@ -3,6 +3,7 @@ import { calculateFareFromRouteSegment, getLineTypeFromFareType } from "./util.s
 import { METRO_STATION, RouteSegment, TravelRoute } from "../types";
 
 export const FareService = {
+    // TODO: refactor remove
     calculate(source: METRO_STATION, destination: METRO_STATION): TravelRoute {
         const routeSegments = graphService.findRoute(source, destination, metroGraph);
 
@@ -21,5 +22,26 @@ export const FareService = {
             route,
             fare: totalFare
         }
+    },
+    findAllRoutes(source: METRO_STATION, destination: METRO_STATION): TravelRoute[] {
+        const routeSegmentsList = graphService.findAllRoutes(source, destination, metroGraph);
+        const travelRoutes = routeSegmentsList.map(routeSegments => {
+            let totalFare = 0;
+            const route = routeSegments.map((routeSegment: RouteSegment) => {
+                const fare = calculateFareFromRouteSegment(routeSegment);
+                const lineType = getLineTypeFromFareType(routeSegment.fareType);
+                totalFare += fare;
+                return {
+                    route: routeSegment.route,
+                    lineType,
+                    fare
+                }
+            });
+            return {
+                route,
+                fare: totalFare
+            }
+        })
+        return travelRoutes;
     }
 }
