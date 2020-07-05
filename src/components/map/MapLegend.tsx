c27600f2470c769df882d9307c68197adf51ff46
HSpotlight <pakawat.ane@gmail.com>
2020-07-05 19:12:38 +0700

    MF-007 Add custom layer control

--- src/components/map/MapLegend.tsx
+++ src/components/map/MapLegend.tsx
@@ -1,9 +1,22 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import "../../styles/MapLegend.scss";
+import { Checkbox } from "@material-ui/core";
+import { MetroLineLayers } from "../MetroMap";
 
-const MapLegend = () => {
+const MapLegend = ({showMetroLineLayers, setShowMetroLayers}: {showMetroLineLayers: MetroLineLayers, setShowMetroLayers: Function}) => {
   const { t: translate } = useTranslation();
+
+  const handleChange = (key: keyof MetroLineLayers) => {
+    setShowMetroLayers((prevState: MetroLineLayers) => {
+      const newState = {
+      ...prevState,
+      }
+      newState[key] = !newState[key];
+      return newState;
+    })
+  }
+
   return (
     <div className="map-legend">
       <div className="title">{translate("map.legend.legend")}</div>
@@ -11,23 +24,35 @@ const MapLegend = () => {
         <LegendLabel
           line="mrt-blue-line"
           title={translate("map.legend.mrtBlueLine")}
+          status={showMetroLineLayers.mrtBlue}
+          onChange={() => handleChange('mrtBlue')}
         />
         <LegendLabel
           line="bts-silom-line"
           title={translate("map.legend.btsSilomLine")}
+          status={showMetroLineLayers.btsSilom}
+          onChange={() => handleChange('btsSilom')}
         />
         <LegendLabel
           line="bts-sukhumvit-line"
           title={translate("map.legend.btsSukhumvitLine")}
+          status={showMetroLineLayers.btsSukhumvit}
+          onChange={() => handleChange('btsSukhumvit')}
         />
       </div>
     </div>
   );
 };
 
-const LegendLabel = ({ line, title }: { line: string; title: string }) => {
+const LegendLabel = ({ line, title, status, onChange }: { line: string; title: string, status: boolean, onChange: any }) => {
   return (
     <div className="legend-key-value">
+      <Checkbox
+        checked={status}
+        onChange={onChange}
+        name={`key ${line}`}
+        color="primary"
+      />
       <div className={`key ${line}`}>
         <div className="line"></div>
         <div className="circle"></div>
