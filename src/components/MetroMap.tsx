9e1a0730de28c72a5caa381f9d3c898a3fcae3ff
HSpotlight <pakawat.ane@gmail.com>
2020-10-20 11:04:18 +0700

    MF-000 Refactor MetroMap

--- src/components/MetroMap.tsx
+++ src/components/MetroMap.tsx
@@ -65,110 +65,101 @@ export const MetroMap = () => {
 
 const MetroLineLayers = () => {
   const { showMetroLineLayers } = useContext(MapContext);
+  const mrtThapraPosition = MRT_BLUE[31].position;
+  const polylineLayers = [{
+    layername: "MRT-blue-line",
+    color: colors.mrtBlue,
+    polyline: [mrtThapraPosition, ...getPolyLineFromStations(MRT_BLUE)],
+    isVisible: true,
+  }, {
+    layername: "BTS-silom-line",
+    color: colors.btsSilom,
+    polyline: getPolyLineFromStations(BTS_SILOM),
+    isVisible: true,
+  }, {
+    layername: "BTS-sukhumvit-line",
+    color: colors.btsSukhumvit,
+    polyline: getPolyLineFromStations(BTS_SUKHUMVIT),
+    isVisible: true,
+  }, {
+    layername: "ARL-line",
+    color: colors.arl,
+    polyline: getPolyLineFromStations(ARL),
+    isVisible: true,
+  }, {
+    layername: "BRT-line",
+    color: colors.brt,
+    polyline: getPolyLineFromStations(BRT),
+    isVisible: true,
+  }]
+  const stationLayers = [{
+    layername: "MRT-blue-station",
+    isVisible: showMetroLineLayers.mrtBlue,
+    color: colors.mrtBlue,
+    stations: MRT_BLUE,
+  }, {
+    layername: "BTS-silom-station",
+    isVisible: showMetroLineLayers.btsSilom,
+    color: colors.btsSilom,
+    stations: BTS_SILOM,
+  }, {
+    layername: "BTS-sukhumvit-station",
+    isVisible: showMetroLineLayers.btsSukhumvit,
+    color: colors.btsSukhumvit,
+    stations: BTS_SUKHUMVIT,
+  }, {
+    layername: "ARL-station",
+    isVisible: showMetroLineLayers.arl,
+    color: colors.arl,
+    stations: ARL,
+  }, {
+    layername: "BRT-station",
+    isVisible: showMetroLineLayers.brt,
+    color: colors.brt,
+    stations: BRT,
+  }]
+
+  const displayPolylineLayers = () => {
+    return polylineLayers.map(polylineLayer => (
+      <LayersControl.Overlay
+        key={polylineLayer.layername}
+        name={polylineLayer.layername}
+        checked={polylineLayer.isVisible}
+      >
+        <FeatureGroup name={polylineLayer.layername}>
+          <Polyline
+            positions={polylineLayer.polyline}
+            color={polylineLayer.color}
+          />
+        </FeatureGroup>
+      </LayersControl.Overlay>
+    ));
+  }
+
+  const displayStationLayers = () => {
+    return stationLayers.map(stationLayer => (
+      <LayersControl.Overlay 
+        key={stationLayer.layername}
+        name={stationLayer.layername} 
+        checked={stationLayer.isVisible}
+        >
+      <FeatureGroup name="mrt-blue-station">
+        {stationLayer.stations.map((station) => (
+          <StationMarker
+            key={station.key}
+            station={station}
+            fillColor={stationLayer.color}
+          />
+        ))}
+      </FeatureGroup>
+      </LayersControl.Overlay>
+    ))
+  }
+
   return (
     <LayersControl position="topright">
-    <LayersControl.Overlay name="MRT Blue Line" checked={true}>
-      <FeatureGroup name="mrt-blue-line">
-        <Polyline
-          positions={getPolyLineFromStations(MRT_BLUE)}
-          color={colors.mrtBlue}
-        />
-        <Polyline
-          positions={[
-            [13.740013, 100.470773],
-            [13.7298, 100.474151],
-          ]}
-          color={colors.mrtBlue}
-        />
-      </FeatureGroup>
-      </LayersControl.Overlay>
-      <LayersControl.Overlay name="BTS Silom Line" checked={true}>
-      <FeatureGroup name="bts-silom-line">
-        <Polyline
-          positions={getPolyLineFromStations(BTS_SILOM)}
-          color={colors.btsSilom}
-        />
-      </FeatureGroup>
-      </LayersControl.Overlay>
-      <LayersControl.Overlay name="BTS Sukhumvit Line" checked={true}>
-      <FeatureGroup name="bts-sukhumvit-line">
-        <Polyline
-          positions={getPolyLineFromStations(BTS_SUKHUMVIT)}
-          color={colors.btsSukhumvit}
-        />
-      </FeatureGroup>
-      </LayersControl.Overlay>
-      <LayersControl.Overlay name="ARL Line" checked={true}>
-      <FeatureGroup name="arl-line">
-        <Polyline
-          positions={getPolyLineFromStations(ARL)}
-          color={colors.arl}
-        />
-      </FeatureGroup>
-      </LayersControl.Overlay>
-      <LayersControl.Overlay name="BRT Line" checked={true}>
-      <FeatureGroup name="brt-line">
-        <Polyline
-          positions={getPolyLineFromStations(BRT)}
-          color={colors.brt}
-        />
-      </FeatureGroup>
-      </LayersControl.Overlay>
-      <LayersControl.Overlay name="MRT Blue Station" checked={showMetroLineLayers.mrtBlue}>
-      <FeatureGroup name="mrt-blue-station">
-        {MRT_BLUE.map((station) => (
-          <StationMarker
-            key={station.key}
-            station={station}
-            fillColor={colors.mrtBlue}
-          />
-        ))}
-      </FeatureGroup>
-      </LayersControl.Overlay>
-      <LayersControl.Overlay name="BTS Silom Station" checked={showMetroLineLayers.btsSilom}>
-      <FeatureGroup name="bts-silom-station">
-        {BTS_SILOM.map((station) => (
-          <StationMarker
-            key={station.key}
-            station={station}
-            fillColor={colors.btsSilom}
-          />
-        ))}
-      </FeatureGroup>
-      </LayersControl.Overlay>
-      <LayersControl.Overlay name="BTS Sukhumvit Station" checked={showMetroLineLayers.btsSukhumvit}>
-      <FeatureGroup name="bts-sukhumvit-station">
-        {BTS_SUKHUMVIT.map((station) => (
-          <StationMarker
-            key={station.key}
-            station={station}
-            fillColor={colors.btsSukhumvit}
-          />
-        ))}
-      </FeatureGroup>
-      </LayersControl.Overlay>
-      <LayersControl.Overlay name="Airport Rail Link Station" checked={showMetroLineLayers.arl}>
-      <FeatureGroup name="arl-station">
-        {ARL.map((station) => (
-          <StationMarker
-            key={station.key}
-            station={station}
-            fillColor={colors.arl}
-          />
-        ))}
-      </FeatureGroup>
-      </LayersControl.Overlay>
-      <LayersControl.Overlay name="BRT Station" checked={showMetroLineLayers.brt}>
-      <FeatureGroup name="brt-station">
-        {BRT.map((station) => (
-          <StationMarker
-            key={station.key}
-            station={station}
-            fillColor={colors.brt}
-          />
-        ))}
-      </FeatureGroup>
-      </LayersControl.Overlay>
+      {displayPolylineLayers()}
+      {displayStationLayers()}
     </LayersControl>
   )
 }
