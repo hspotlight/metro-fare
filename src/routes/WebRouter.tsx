2c93a19588da2ada70064d70697e2a63b881e417
HSpotlight <pakawat.ane@gmail.com>
2020-12-09 16:20:30 +0700

    MF-010 Add HeaderBar

--- src/routes/WebRouter.tsx
+++ src/routes/WebRouter.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import HeaderBar from "../components/HeaderBar";
 import RouteNavigation from "../components/RouteNavigation";
 
 const HomePage = () => (
-  <>
+  <div style={{ display: 'flex', flexDirection: 'column', alignSelf: "flex-start"}}>
+    <HeaderBar title={"MetroFare"} />
     <RouteNavigation />
-  </>
+  </div>
 );
 
 export const WebRouter = () => {
