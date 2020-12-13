67df3fbc555f7212ae030a06f52599db759e9f29
HSpotlight <pakawat.ane@gmail.com>
2020-12-13 14:54:34 +0700

    MF-016 Handle refresh and navigate in routes and route detail

--- src/components/RoutesSearchResult.tsx
+++ src/components/RoutesSearchResult.tsx
@@ -1,38 +1,59 @@
 import React, { useEffect, useState } from "react";
 import { useTripContext } from "../contexts/TripProvider";
 import FareService from "../services/fare.service";
-import { TravelRoute } from "../types";
+import { METRO_STATION, TravelRoute } from "../types";
 import RouteFromTo from "./RouteFromTo";
 import RouteInfoCard from "./RouteInfoCard";
-import { useHistory } from 'react-router-dom'
+import { useHistory, useLocation } from "react-router-dom";
+import { getStation } from "../services/util.service";
 
 const RoutesSearchResult = () => {
-  const history = useHistory()
-  const { trip, setTravelRoute } = useTripContext();
-  const [travelRoutes, setTravelRoutes] = useState<TravelRoute[]>([])
+  const history = useHistory();
+  const { trip, setTravelRoute, setJourney } = useTripContext();
+  const [travelRoutes, setTravelRoutes] = useState<TravelRoute[]>([]);
+  const query = new URLSearchParams(useLocation().search);
+
+  useEffect(() => {
+    const source = query.get("source");
+    const destination = query.get("destination");
+    if (getStation(source as METRO_STATION) && getStation(destination as METRO_STATION)) {
+      setJourney(source as METRO_STATION, destination as METRO_STATION);
+    } else {
+      history.replace('/')
+    }
+  }, []);
 
   useEffect(() => {
     if (trip.source && trip.destination) {
-      const travelRoutes = FareService.findAllRoutes(trip.source, trip.destination)
+      const travelRoutes = FareService.findAllRoutes(
+        trip.source,
+        trip.destination
+      )
         .sort((a, b) => a.fare - b.fare)
         .slice(0, 5);
-      setTravelRoutes(travelRoutes)
+      setTravelRoutes(travelRoutes);
     }
-  }, [trip])
+  }, [trip]);
 
-  const handleViewRouteDetail = (travelRoute: TravelRoute) => {
-    setTravelRoute(travelRoute)
-    history.push('route-detail')
-  }
+  const handleViewRouteDetail = (travelRoute: TravelRoute, index: number) => {
+    setTravelRoute(travelRoute);
+    history.push(`route-detail?source=${travelRoute.source}&destination=${travelRoute.destination}&&route=${index}`);
+  };
 
   return (
     <div>
       <RouteFromTo departure={trip.source} arrival={trip.destination} />
       {travelRoutes.length > 0 &&
         travelRoutes.map((travelRoute, index) => {
-          return <RouteInfoCard key={index} travelRoute={travelRoute} title={'Route ' + (index + 1)} onClick={handleViewRouteDetail} />
-        })
-      }
+          return (
+            <RouteInfoCard
+              key={index}
+              travelRoute={travelRoute}
+              title={"Route " + (index + 1)}
+              onClick={() => handleViewRouteDetail(travelRoute, index)}
+            />
+          );
+        })}
     </div>
   );
 };
