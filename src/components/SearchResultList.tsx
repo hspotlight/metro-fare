9a85db99603bbff7f84395cacfe5b74806cebe5f
HSpotlight <pakawat.ane@gmail.com>
2021-04-04 19:28:34 +0700

    MF-031 adjust showmore style

--- src/components/SearchResultList.tsx
+++ src/components/SearchResultList.tsx
@@ -2,19 +2,25 @@ import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { getLineTypeLabel, getStationName } from "../services/util.service";
 import { METRO_STATION_ID, Station } from "../types";
-import "../styles/SearchResultList.scss";
 import {
   Button,
   List,
   ListItem,
   ListItemText,
   makeStyles,
+  Paper,
+  Typography,
 } from "@material-ui/core";
 
 const useStyles = makeStyles(() => ({
   listItem: {
     paddingLeft: "8px",
     paddingRight: "8px",
+  },
+  showMoreContainer: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
   },
 }));
 
@@ -29,9 +35,9 @@ export const SearchResultList = ({
   searchItems,
   handleOnItemClick,
 }: SearchResultListProps) => {
+  const classes = useStyles();
   const searchItemLength = searchItems.length;
   const [showMoreButton, setShowMoreButton] = useState<boolean>(false);
-
   const [showMoreItems, setShowMoreItems] = useState<boolean>(false);
 
   const topTenItems = searchItems.slice(0, limit);
@@ -47,20 +53,26 @@ export const SearchResultList = ({
   }, [searchItemLength]);
 
   return (
-    <div className="search-result-list">
-      <List>
-        {topTenItems.map((item, index) => (
-          <SearchItem key={index} item={item} onClick={handleOnItemClick} />
-        ))}
-        {showMoreItems &&
-          moreItems.map((item, index) => (
+    <>
+      <Paper>
+        <List>
+          {topTenItems.map((item, index) => (
             <SearchItem key={index} item={item} onClick={handleOnItemClick} />
           ))}
-      </List>
+          {showMoreItems &&
+            moreItems.map((item, index) => (
+              <SearchItem key={index} item={item} onClick={handleOnItemClick} />
+            ))}
+        </List>
+      </Paper>
       {showMoreButton && (
-        <Button onClick={handleClickShowMore}>Show More</Button>
+        <div className={classes.showMoreContainer}>
+          <Button onClick={handleClickShowMore}>
+            <Typography variant="button">Show More</Typography>
+          </Button>
+        </div>
       )}
-    </div>
+    </>
   );
 };
 
