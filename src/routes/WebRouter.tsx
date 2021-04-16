4b0424567ff2a46fcb2a2dca8c0e1d22fee40ea5
HSpotlight <pakawat.ane@gmail.com>
2021-04-16 12:55:22 +0700

    Remove RouteNavigation and HeaderBar

--- src/routes/WebRouter.tsx
+++ src/routes/WebRouter.tsx
@@ -1,38 +1,14 @@
-import React, { useEffect } from "react";
-import { Route, Switch, useHistory } from "react-router-dom";
-import HeaderBar from "../components/HeaderBar";
-import RouteNavigation from "../components/RouteNavigation";
-
-const pageWithHeaderStyle: React.CSSProperties = {
-  display: "flex",
-  flexDirection: "column",
-  alignSelf: "flex-start",
-  width: "100%",
-};
-
-const HomePage = () => (
-  <div style={pageWithHeaderStyle}>
-    <HeaderBar title={"MetroFare"} />
-    <RouteNavigation />
-  </div>
-);
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
 const RedirectToHomePage = () => {
-  const history = useHistory();
-
-  useEffect(() => {
-    history.replace("/");
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return <div></div>;
 };
 
 export const WebRouter = () => {
   return (
     <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route component={RedirectToHomePage} />
+      <Route exact path="/" component={RedirectToHomePage} />
     </Switch>
   );
 };
