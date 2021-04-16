6a60f235323b13fac0ccec995dd83e3d8a8b5842
HSpotlight <pakawat.ane@gmail.com>
2021-04-16 12:50:41 +0700

    Remove btsFare api call

--- src/services/btsFare.service.ts
+++ src/services/btsFare.service.ts
@@ -1,25 +1,12 @@
-import axios from "axios";
 import { METRO_STATION_ID } from "../types";
-import qs from 'querystring';
-import { BTSFareResponse } from "../types/BTSFareResponse";
 import { BTS_ID } from "../data/btsId";
 import btsFareTable from './btsFareTable.json';
-
-const BTS_BASE_URL = "https://btsapp1.bts.co.th/webservice/api/";
-
-const api = axios.create({
-    baseURL: BTS_BASE_URL,
-    timeout: 2000,
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-    }
-})
 
 export const getBTSFareFromTable = (from: METRO_STATION_ID, to: METRO_STATION_ID): number => {
     // NOTE: order of the btsID objects must not change
     const fromIndex = BTS_ID.findIndex(btsStation => btsStation.id === from)
     const toIndex = BTS_ID.findIndex(btsStation => btsStation.id === to)
-    
+
     try {
         let fare = 0;
         if (fromIndex <= toIndex) {
@@ -28,22 +15,7 @@ export const getBTSFareFromTable = (from: METRO_STATION_ID, to: METRO_STATION_ID
             fare = btsFareTable[to][from];
         }
         return fare;
-    } catch(e) {
+    } catch (e) {
         return 0;
     }
-}
-
-export const getBTSFare = async (from: METRO_STATION_ID, to: METRO_STATION_ID): Promise<number> => {
-    const originId = BTS_ID.find(btsStation => btsStation.id === from)?.btsId || 0
-    const destinationId = BTS_ID.find(btsStation => btsStation.id === to)?.btsId || 0
-
-    const response: BTSFareResponse = await api.post('gatFareRate', qs.stringify({
-        'Origin': '' + originId,
-        'Destination': '' + destinationId
-    })).then(response => response.data)
-
-    if (response && response.FareRate.length > 0) {
-        return +response.FareRate[0].FarePrice
-    }
-    return 0;
 }
