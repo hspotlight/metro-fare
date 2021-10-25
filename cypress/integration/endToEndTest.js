31919238cee46d06f04ca8dc6d1f49a6ef64a9fc
Miewliie <miewliie@gmail.com>
2021-10-25 23:47:41 +0700

    E2E test set up

---
+++ cypress/integration/endToEndTest.js
@@ -0,0 +1,7 @@
+/// <reference types="Cypress" />
+
+describe('Validate access url', function(){
+    it('My FirstTest case', function(){
+        cy.visit('https://metrofare.netlify.app/')
+    })
+})
